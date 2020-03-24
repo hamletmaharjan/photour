1a5c744daf32451ad83fd8a4625b817ae16cbd7c
hamlet <imhamlet.hams@gmail.com>
2020-03-25 00:05:42 +0545

    prep for vue router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
 import store from './store/index';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: [{path: '/oauth/callback', component: AuthHandler s}]
+});
 
 
 new Vue({
