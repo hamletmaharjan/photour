89c65db72c37ee7c9d044bad44ddb671826d5eb5
hamlet <imhamlet.hams@gmail.com>
2020-03-25 00:17:16 +0545

    switched from hash to browser router

--- src/main.js
+++ src/main.js
@@ -2,15 +2,18 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App';
 import store from './store/index';
+import AuthHandler from './components/AuthHandler';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-    routes: [{path: '/oauth/callback', component: AuthHandler s}]
+    mode: 'history',
+    routes: [{path: '/oauth/callback', component: AuthHandler }]
 });
 
 
 new Vue({
+    router,
     store: store,
     render: h=>h(App)
 }).$mount('#app');
