453f20eda96cfe2146e88abdda41559dd90352a3
hamlet <imhamlet.hams@gmail.com>
2020-03-25 12:57:49 +0545

    log in and out

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,32 @@
 <template>
     <div class="ui secondary pointing menu">
-        <a href="/" class="active item">Photour</a>
+        <router-link to="/" class="active item">Photour</router-link>
 
         <div class="right menu">
-            <a href="#" class="ui item" v-on:click="login">Login</a>
+            <div v-if="isLoggedIn" class="horizontal">
+                <router-link to="/" class="item">Galleries</router-link>
+                <router-link to="/upload" class="item">Upload</router-link>
+                <a class="item" v-on:click="logout">Logout</a>
+
+            </div>
+            <a v-else href="#" class="ui item" v-on:click="login">Login</a>
         </div>
     </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 export default {
     name: 'AppHeader',
-    methods: mapActions(['login'])
+    computed: mapGetters(['isLoggedIn']),
+    methods: mapActions(['login', 'logout'])
 }
 </script>
+
+
+<style scoped>
+.horizontal {
+    display: flex;
+    flex-direction: row;
+}
+</style>
